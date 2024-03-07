03d7af086f333ad1751a8dcb697e0902982f01e0
Kale13-R <Kalebriley913@yahoo.com>
2024-03-07 13:41:59 -0600

    Initial Commit

---
+++ XUBOTB24/XUBOTB24App.swift
@@ -0,0 +1,17 @@
+//
+//  XUBOTB24App.swift
+//  XUBOTB24
+//
+//  Created by Kaleb Riley on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct XUBOTB24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
